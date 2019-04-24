28b03eb9732b0d6a414ad56c8736357948e638a3
panwei <948567749@qq.com>
2019-04-24 19:21:41 +0800

    first day

--- PWVideoPlayer/PWVideoPlayer/Source/PWVideoPlayer.h
+++ PWVideoPlayer/PWVideoPlayer/Source/PWVideoPlayer.h
@@ -7,11 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface PWVideoPlayer : NSObject
-
++ (instancetype)playVideoWithURL:(NSString *)remoteURL inViewController:(UIViewController *)superViewController frame:(CGRect)frame;
 @end
 
 NS_ASSUME_NONNULL_END
--- PWVideoPlayer/PWVideoPlayer/Source/PWVideoPlayer.m
+++ PWVideoPlayer/PWVideoPlayer/Source/PWVideoPlayer.m
@@ -7,7 +7,26 @@
 //
 
 #import "PWVideoPlayer.h"
+#import <AVFoundation/AVFoundation.h>
 
 @implementation PWVideoPlayer
++ (instancetype)playVideoWithURL:(NSString *)remoteURL inViewController:(UIViewController *)superViewController frame:(CGRect)frame {
+    PWVideoPlayer *player = [[[self class] alloc] init];
+    AVPlayer *avPlayer = [[AVPlayer alloc] initWithURL:[NSURL URLWithString:remoteURL]];
+    AVPlayerLayer *playerLayer = [AVPlayerLayer playerLayerWithPlayer:avPlayer];
+    playerLayer.frame = frame;
+    [superViewController.view.layer addSublayer:playerLayer];
+    [avPlayer play];
+    return player;
+}
+
+- (instancetype)initWithURL:(nullable NSURL *)fileURL  {
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+    return self;
+}
+
 
 @end
