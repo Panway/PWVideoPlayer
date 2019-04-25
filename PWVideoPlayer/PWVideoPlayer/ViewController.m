275bfe22ab46f49c40848d9bad6ed9ddc7ed3f24
apple <944261495@qq.com>
2019-04-25 18:24:27 +0800

    update demo
    update demo

--- PWVideoPlayer/PWVideoPlayer/ViewController.m
+++ PWVideoPlayer/PWVideoPlayer/ViewController.m
@@ -22,15 +22,19 @@
     // Do any additional setup after loading the view.
 //    [PWVideoPlayer playVideoWithURL:@"https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/f93425c71a06e1520fe067ead30b2dea.mp4" inViewController:self frame:CGRectMake(0, 120, 375, 200)];
     
-//    UIView * palyView = [[UIView alloc]init];
-//    palyView.frame = CGRectMake(20, 30, 300, 100);
-//    palyView.backgroundColor = UIColor.lightGrayColor;
-//    [self.view addSubview:palyView];
+    UIView * palyView = [[UIView alloc]init];
+    palyView.frame = CGRectMake(20, 100, 300, 200);
+    palyView.backgroundColor = UIColor.lightGrayColor;
+    [self.view addSubview:palyView];
 
 
     self.player = [[PWPlayer alloc]init];
 
-[self.player playWithUrl:@"https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/f93425c71a06e1520fe067ead30b2dea.mp4" playView:self.view];
+[self.player   playWithUrl:@"https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/f93425c71a06e1520fe067ead30b2dea.mp4" playView:palyView];
+
+
+
+
 }
 
 
